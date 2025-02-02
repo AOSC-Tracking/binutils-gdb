072457058d06965a634c35c44b6ecc3c62609e56
Mingcong Bai <jeffbai@aosc.io>
2025-02-02 23:59:57 +0800

    AOSCOS: bfd: (arm64, powerpc, ppc64, ppc64el) set common page size to 64KiB
    According to Fedora's binutils spec, "On ppc64 and aarch64 (arm64), we
    might use 64KiB pages."
    
    Link: https://src.fedoraproject.org/rpms/binutils/blob/53dfb5f3d99d3ecd5118d5e3957ab48bdc6db74f/f/binutils.spec#_577
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -10622,7 +10622,7 @@ const struct elf_size_info elfNN_aarch64_size_info =
 #define ELF_TARGET_ID			AARCH64_ELF_DATA
 #define ELF_MACHINE_CODE		EM_AARCH64
 #define ELF_MAXPAGESIZE			0x10000
-#define ELF_COMMONPAGESIZE		0x1000
+#define ELF_COMMONPAGESIZE		0x10000
 
 #define bfd_elfNN_bfd_is_target_special_symbol	\
   elfNN_aarch64_is_target_special_symbol
